9766a3ec773a3d08abbf5d0d093779c17f8e60ed
loboy <loboyoh@pchome.com.tw>
2016-03-07 20:28:11 +0800

    Button_onClick (20160307)
    public void submit(View view)

--- MyApplication/app/src/main/java/com/example/loboy/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/loboy/myapplication/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.loboy.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void  submit(View view)
+    {
+        Toast.makeText(this, "Hello World 20160307!!!", Toast.LENGTH_LONG).show();
     }
 }
